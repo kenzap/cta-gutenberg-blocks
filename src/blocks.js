3bc68dc8fd44387f73e87dfea00f738ae8a6f796
Alex <info@kenzap.com>
2019-11-19 22:48:51 +0700

    Add files via upload

---
+++ src/blocks.js
@@ -0,0 +1,36 @@
+/**
+ * Gutenberg Blocks
+ *
+ * All blocks related JavaScript files should be imported here.
+ * You can create a new block folder in this dir and include code
+ * for that block here as well.
+ *
+ * All blocks should be included here since this is the file that
+ * Webpack is compiling as the input file.
+ */
+
+import './cta-1/block.js';
+import './cta-2/block.js';
+import './cta-3/block.js';
+import './cta-4/block.js';
+import './cta-5/block.js';
+import './cta-6/block.js';
+import './cta-7/block.js';                                                                                                                               
+import './cta-8/block.js';                                                                                                                               
+import './cta-9/block.js';                                                                                                                               
+import './cta-10/block.js';                                                                                                                               
+import './cta-11/block.js';                                                                                                                               
+import './cta-12/block.js';                                                                                                                               
+import './cta-13/block.js';                                                                                                                               
+import './cta-14/block.js';                                                                                                                               
+import './cta-15/block.js';                                                                                                                               
+import './cta-16/block.js';                                                                                                                               
+import './cta-17/block.js';                                                                                                                               
+import './cta-18/block.js';                                                                                                                               
+import './cta-19/block.js';                                                                                                                               
+import './cta-20/block.js';                                                                                                                               
+import './cta-21/block.js';                                                                                                                               
+import './cta-22/block.js';                                                                                                                               
+import './cta-23/block.js';                                                                                                                               
+import './cta-24/block.js';                                                                                                                               
+import './cta-25/block.js';                                                                                                                               
